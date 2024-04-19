9ae5cdbdb4c799b14bc4ebf0deb0aeae6fc25cf6
Roberto Vetere <Roberto.vetere@gmail.com>
2024-04-19 11:46:48 +0200

    first commit

---
+++ src/main/java/com/holidevs/createlogin/CreateLoginApplication.java
@@ -0,0 +1,13 @@
+package com.holidevs.createlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateLoginApplication.class, args);
+	}
+
+}
